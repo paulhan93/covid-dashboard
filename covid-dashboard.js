0ce0fcfb672a341602f84ea816feb0ded3e607fc
paul <kihan@pdx.edu>
2021-05-21 22:35:09 -0700

    Update covid-dashboard.js

--- covid-dashboard.js
+++ covid-dashboard.js
@@ -1,34 +1,36 @@
+// API urls
 let url = "https://disease.sh/v3/covid-19/historical/all?lastdays=all";
 
-
-
+// maybe make into more general converting function (raw -> json)
 // get data of total new cases, recovered, and deaths. (Bar/Line graph)
 async function getAllData() {
-    const rawData = await fetch(url);
-    if(rawData == undefined) {
-        console.log("Error.")
-    }
-    const jsonData = await rawData.json();
+  const rawData = await fetch(url);
+  if (rawData == undefined) {
+    console.log("Error.");
+  }
+  const jsonData = await rawData.json();
 
-    return jsonData;
+  return jsonData;
 }
 
+// manipulate above data to display in line graph
+function createLineGraph() {
+  
 
-
-
-const config = {
-    type: 'line',
+  const linegraph = {
+    type: "line",
     data: data,
     options: {
       responsive: true,
       plugins: {
         legend: {
-          position: 'top',
+          position: "top",
         },
         title: {
           display: true,
-          text: 'Chart.js Line Chart'
-        }
-      }
+          text: "Chart.js Line Chart",
+        },
+      },
     },
   };
+}
