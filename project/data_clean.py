f0acd30a1834116de37321b9c0ecf707c5b89e15
Sara <alot4@pdx.edu>
2021-05-29 09:26:17 -0700

    data_clean

--- project/data_clean.py
+++ project/data_clean.py
@@ -12,3 +12,17 @@ covid_clean = covid[['date','county','state','cases']]
 covid_clean.to_json('oregon.json', index=False)
 covid_clean_new = pd.read_json('oregon.json')
 covid_clean_new.head()
+# Get the data of counties from oregon.json file
+
+oregon_covid = pd.read_json('oregon.json')
+
+data = []
+
+for i in range(len(oregon_covid)):
+  if oregon_covid.iloc[i].state == 'Oregon':
+    data.append(oregon_covid.iloc[i])
+
+oregon_data = pd.DataFrame(data, columns = ['date', 'county', 'state', 'cases'])
+oregon_data.to_json('only_for_oregon_counties.csv', index=False)
+oregon_data.head()
+
