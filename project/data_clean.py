bcb55dc40c7a50a9fec6a07a7ae0e5f91c7795a2
Sara <alot4@pdx.edu>
2021-05-29 09:22:11 -0700

    data_clean

--- project/data_clean.py
+++ project/data_clean.py
@@ -5,3 +5,10 @@ import seaborn as sns
 import math
 import datetime
 import csv
+covid = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv')
+covid.head()
+# Data Clean
+covid_clean = covid[['date','county','state','cases']]
+covid_clean.to_json('oregon.json', index=False)
+covid_clean_new = pd.read_json('oregon.json')
+covid_clean_new.head()
